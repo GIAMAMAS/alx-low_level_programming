7cd657ca0d684d5ec2c553436b47e9dd8590e464
GIAMAMAS <amankwagideonkyei321@gmail.com>
2023-06-18 13:57:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main -  printing to standard output with puts
  *
  * Return: 0 (success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
